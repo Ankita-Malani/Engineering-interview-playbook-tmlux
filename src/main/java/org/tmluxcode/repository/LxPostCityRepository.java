f6d4c88f9c4d3799b9f7f9f2ebe3f03ebe138569
AnkitaMalani <anki546.malani@gmail.com>
2026-05-07 21:38:34 +0300

    Imports updated

--- src/main/java/org/tmluxcode/repository/LxPostCityRepository.java
+++ src/main/java/org/tmluxcode/repository/LxPostCityRepository.java
@@ -1,9 +1,7 @@
 package org.tmluxcode.repository;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.tmluxcode.dto.LxPostCity;
 
 @Repository
 @Slf4j
